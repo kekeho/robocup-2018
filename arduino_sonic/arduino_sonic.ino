2b266a63ccbea86ad75ef6d34bc7c08ed5911f5b
Hiroki.T <hirodora@me.com>
2018-02-25 15:12:08 +0900

    add: 3つ同時に計測 なお速度がおそすぎる

--- arduino_sonic/arduino_sonic.ino
+++ arduino_sonic/arduino_sonic.ino
@@ -1,30 +1,42 @@
 int fst_trig = 3;
 int fst_echo = 2;
+int snd_trig = 5;
+int snd_echo = 6;
+int thd_trig = 11;
+int thd_echo = 12;
+
+float fst_dist; float snd_dist; float thd_dist;
 
 void setup() {
   Serial.begin(9600);
   pinMode(fst_trig, OUTPUT);
   pinMode(fst_echo, INPUT);
+  pinMode(snd_trig, OUTPUT);
+  pinMode(snd_echo, INPUT);
+  pinMode(thd_trig, OUTPUT);
+  pinMode(thd_echo, INPUT);
 }
 
 void loop() {
-  sonic_distance(fst_trig, fst_echo);
+  fst_dist = sonic_distance(fst_trig, fst_echo);
+  snd_dist = sonic_distance(snd_trig, snd_echo);
+  thd_dist = sonic_distance(thd_trig, thd_echo);
+  Serial.print("1. ");
+  Serial.println(fst_dist);
+  Serial.print("2. ");
+  Serial.println(snd_dist);
+  Serial.print("3. ");
+  Serial.println(thd_dist);
 }
 
-void sonic_distance(int trig, int echo){
+float sonic_distance(int trig, int echo){
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
   float duration = pulseIn(echo, HIGH);
-  if(duration > 0){
-    duration = duration/2;
-    float distance = duration*340*100/1000000;
-    Serial.print("                                      "); //初期化
-    Serial.print("\rDistance: ");
-    Serial.print(distance);
-    Serial.print("cm");
-  }
-  delay(500);
+  duration = duration/2;
+  float distance = duration*340*100/1000000;
+  return distance;
 }
