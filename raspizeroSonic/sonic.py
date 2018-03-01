9cbbcc5934aa623ca216201ce4f1f4cf772546d4
Hiroki.T <hirodora@me.com>
2018-03-01 20:09:06 +0900

    change: wiringpiで書き直した

--- raspizeroSonic/sonic.py
+++ raspizeroSonic/sonic.py
@@ -1,58 +1,60 @@
-import RPi.GPIO as gpio
-import time
+import wiringpi as pi
+import time, sys
 
-fst_trig = 0
-fst_echo = 1
-snd_trig = 2
-snd_echo = 3
-thd_trig = 4
-thd_echo = 5
+# 番号はGPIO番号であってPIN番号ではない
+fst_trig = 4
+fst_echo = 5
+snd_trig = 6
+snd_echo = 7
+thd_trig = 8
+thd_echo = 9
 HIGH = 1
 LOW = 0
 
 
 def setup(trig, echo):
-    gpio.setup(trig, gpio.OUT)
-    gpio.setup(echo, gpio.IN)
-
-
-def pulseIn(pin):
-    global HIGH
-    t_start = 0
-    t_end = 0
-
-    # check high time
-    while gpio.input(pin) == HIGH:
-        t_end = time.time()
-    return t_end - t_start
+    pi.pinMode(trig, pi.OUTPUT)
+    pi.pinMode(echo, pi.INPUT)
 
 
 def sonic_dist(trig, echo):
-    global HIGH
-    global LOW
-    gpio.output(trig, LOW)
+    pi.digitalWrite(trig, pi.LOW)
     time.sleep(2 * 10**(-6))  # wait 2μs
-    gpio.output(trig, HIGH)
+    pi.digitalWrite(trig, pi.HIGH)
     time.sleep(10 * 10**(-6))  # wait 10μs
-    gpio.output(trig, LOW)
-    duration = pulseIn(echo, HIGH)
-    duration = duration / 2
-    dist = duration * 340 * 100 / 1000000
+    pi.digitalWrite(trig, pi.LOW)
+    sigoff = time.time()
+    while(pi.digitalRead(echo) == pi.LOW):
+        sigoff = time.time()
+    sigon = time.time()
+
+    dist = ((sigon - sigoff) * 34000) / 2
     return dist
 
 
 def main():
-    gpio.setmode(gpio.BOARD)
-    setup(fst_trig, fst_echo)
-    setup(snd_trig, snd_echo)
-    setup(thd_trig, thd_echo)
-
     fst_dist = sonic_dist(fst_trig, fst_echo)
-    snd_dist = sonic_dist(snd_trig, snd_echo)
-    thd_dist = sonic_dist(thd_trig, thd_echo)
-    print(fst_dist, snd_dist, thd_dist)
+    # snd_dist = sonic_dist(snd_trig, snd_echo)
+    # thd_dist = sonic_dist(thd_trig, thd_echo)
+    print(fst_dist)
 
 
 if __name__ == '__main__':
-    while True:
-        main()
+    if(sys.argv[1] == '-d'):
+        try:
+            pi.wiringPiSetupGpio()
+            setup(fst_trig, fst_echo)
+            setup(snd_trig, snd_echo)
+            setup(thd_trig, thd_echo)
+            while True:
+                main()
+        except Exception as e:
+            print(e)
+            exit()
+    else:
+        pi.wiringPiSetupGpio()
+        setup(fst_trig, fst_echo)
+        setup(snd_trig, snd_echo)
+        setup(thd_trig, thd_echo)
+        while True:
+            main()
